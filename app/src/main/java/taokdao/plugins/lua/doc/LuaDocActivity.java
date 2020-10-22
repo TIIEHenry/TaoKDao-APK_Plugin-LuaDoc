95dd47d46e434deda531d9dcf6f1029d779edaed
TIIEHenry <1806968131@qq.com>
2020-10-22 20:33:19 +0800

    adapte new version

--- app/src/main/java/taokdao/plugins/lua/doc/LuaDocActivity.java
+++ app/src/main/java/taokdao/plugins/lua/doc/LuaDocActivity.java
@@ -2,15 +2,20 @@ package taokdao.plugins.lua.doc;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.just.agentweb.AgentWeb;
+import com.just.agentweb.WebChromeClient;
+import com.just.agentweb.WebViewClient;
 
 import java.io.File;
 
@@ -22,6 +27,27 @@ public class LuaDocActivity extends AppCompatActivity implements MenuItem.OnMenu
     private long mTouchTime;
     private AgentWeb.PreAgentWeb mAgentWeb;
 
+    private WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url= request.getUrl().toString();
+            if (url.startsWith("file://")) {
+                mAgentWeb.go(url);
+                return true;
+            }
+//            return shouldOverrideUrlLoading(view,.toString());
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.startsWith("file://")) {
+                mAgentWeb.go(url);
+                return true;
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -35,8 +61,10 @@ public class LuaDocActivity extends AppCompatActivity implements MenuItem.OnMenu
         mAgentWeb = AgentWeb.with(this)
                 .setAgentWebParent(mdView, new LinearLayout.LayoutParams(-1, -1))
                 .useDefaultIndicator()
+                .setWebViewClient(mWebViewClient)
                 .createAgentWeb()
                 .ready();
+
         mAgentWeb.get().getAgentWebSettings().getWebSettings().setUseWideViewPort(false);
 
         File file = new File(Constant.extractedDir, "luadoc/manual.html");
@@ -46,6 +74,24 @@ public class LuaDocActivity extends AppCompatActivity implements MenuItem.OnMenu
             mAgentWeb.go("file:///android_asset/luadoc/manual.html");
 //            Toast.makeText(this, file.toString() + " does not exists", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAgentWeb.get().getWebLifeCycle().onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAgentWeb.get().getWebLifeCycle().onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAgentWeb.get().getWebLifeCycle().onDestroy();
     }
 
     @Override
@@ -78,8 +124,12 @@ public class LuaDocActivity extends AppCompatActivity implements MenuItem.OnMenu
     public boolean onMenuItemClick(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.string.catlog:
-                File file = new File(Constant.extractedDir, "luadoc/contents.html#contents");
-                mAgentWeb.go("file://" + file.getAbsolutePath());
+                File file = new File(Constant.extractedDir, "luadoc/contents.html");
+                String url="file:///android_asset/luadoc/contents.html#contents";
+                if (file.isFile()) {
+                    url="file://" + file.getAbsolutePath() + "#contents";
+                }
+                mAgentWeb.go(url);
                 break;
             case R.string.back:
                 onBackPressed();
